25dca70d5520f9105fe1ed6032c70f4d35c21f1f
noud <noud4@home.nl>
2021-04-04 18:22:34 +0200

    models

---
+++ src/app/Models/NormalNoun.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Nouns\Models;
+
+use Illuminate\Database\Eloquent\Model as Model;
+
+
+
+/**
+ * Class NormalNoun
+ * @package App\Models
+ * @version April 4, 2021, 4:16 pm UTC
+ *
+ * @property \Illuminate\Database\Eloquent\Collection $normalNounReferences
+ * @property string $word
+ * @property string $rule_id
+ * @property string $irregular
+ */
+class NormalNoun extends Model
+{
+
+
+    public $table = 'normal_nouns';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+
+
+    public $fillable = [
+        'word',
+        'rule_id',
+        'irregular'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'word' => 'string',
+        'rule_id' => 'string',
+        'irregular' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'word' => 'required|string|max:255',
+        'rule_id' => 'required|string|max:255',
+        'irregular' => 'required|string|max:255'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     **/
+    public function normalNounReferences()
+    {
+        return $this->hasMany(\App\Models\NormalNounReference::class, 'normal_noun_id');
+    }
+}
