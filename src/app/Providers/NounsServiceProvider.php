a4b8c0368dea87a34e57f9e61f53690ce4bfa811
noud <noud4@home.nl>
2021-04-04 18:14:53 +0200

    no api platform

--- src/app/Providers/NounsServiceProvider.php
+++ src/app/Providers/NounsServiceProvider.php
@@ -2,20 +2,12 @@
 
 namespace Nouns\Providers;
 
-use API\Platform\Console\Commands\Generate;
 use Illuminate\Support\ServiceProvider;
 
 class NounsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                Generate::class,
-            ]);
-        }
-
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations/2022_08_28_214054_create_tables_table.php');
         $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
 
